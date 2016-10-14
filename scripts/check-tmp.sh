b17eaad27436957ca0eb1ffa970f38b353ebd828
Louis Munro <lmunro@inverse.ca>
2016-10-14 01:04:32 -0400

    Fixed engrish.

--- scripts/check-tmp.sh
+++ scripts/check-tmp.sh
@@ -4,7 +4,7 @@
 MAX_TMP_FILES="${MAX_TMP_FILES:-"1000"}"
 
 if [ `find /tmp/ -maxdepth 1 -type f | wc -l` -gt $MAX_TMP_FILES ] ; then
-  echo "There are too much temp files in /tmp"
+  echo "There are too many temp files in /tmp"
   exit 1
 fi
 
