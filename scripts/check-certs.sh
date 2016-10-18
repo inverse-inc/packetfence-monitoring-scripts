32e7602734b169e798822d3a123e3a526cdeff86
Derek Wuelfrath <dwlfrth@users.noreply.github.com>
2016-10-18 16:23:24 -0400

    Typo

--- scripts/check-certs.sh
+++ scripts/check-certs.sh
@@ -9,7 +9,7 @@ CERTS=()    # Will hold "due to expire" certificates
 # Checks for 'certificate_file =' in '/usr/local/pf/raddb/mods-enabled/eap'
 files=($(awk '/certificate_file =/{print substr($0, index($0,$3))}' /usr/local/pf/raddb/mods-enabled/eap))
 
-# HTTPd 
+# HTTPd certificates
 # Checks for 'SSLCertificateFile' in '/usr/local/pf/var/conf/ssl-certificates.conf'
 files+=($(awk '/SSLCertificateFile/{print substr($0, index($0,$2))}' /usr/local/pf/var/conf/ssl-certificates.conf))
 
