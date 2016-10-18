e0ef187d7102abb05b34ede9a2a022e4faea27b3
Durand Fabrice <fdurand@inverse.ca>
2016-10-18 09:25:06 -0400

    Test if mysql is there first

--- scripts/check-innodb-file-per-table.sh
+++ scripts/check-innodb-file-per-table.sh
@@ -6,17 +6,23 @@ DB_PWD=$(perl -I/usr/local/pf/lib -Mpf::db -e 'print $pf::db::DB_Config->{pass}'
 DB_NAME=$(perl -I/usr/local/pf/lib -Mpf::db -e 'print $pf::db::DB_Config->{db}');
 DB_HOST=$(perl -I/usr/local/pf/lib -Mpf::db -e 'print $pf::db::DB_Config->{host}');
 
-# check if MySQL or MariaDB is installed
-if mysql -V | grep -q "MariaDB"; then
-    SQL_ENGINE='mariadb'
-else
-    SQL_ENGINE="mysqld"
-fi
+if ! [ -x "$(command -v mysql)" ]; then
+    # check if MySQL or MariaDB is installed
+    if mysql -V | grep -q "MariaDB"; then
+        SQL_ENGINE='mariadb'
+    else
+        SQL_ENGINE="mysqld"
+    fi
 
-if [ -f /var/run/$SQL_ENGINE/$SQL_ENGINE.pid ]; then
-    INNODB=`mysql -h $DB_HOST -u $DB_USER -p$DB_PWD -D $DB_NAME -e "select 1 from information_schema.GLOBAL_VARIABLES where VARIABLE_NAME=\"INNODB_FILE_PER_TABLE\" and VARIABLE_VALUE=\"ON\";"`
-    if [ -z "$INNODB" ]; then
-        exit 1
+
+    # is MySQL running? meaning we are the live packetfence
+    if [ -f /var/run/$SQL_ENGINE/$SQL_ENGINE.pid ]; then
+        INNODB=`mysql -h $DB_HOST -u $DB_USER -p$DB_PWD -D $DB_NAME -e "select 1 from information_schema.GLOBAL_VARIABLES where VARIABLE_NAME=\"INNODB_FILE_PER_TABLE\" and VARIABLE_VALUE=\"ON\";"`
+        if [ -z "$INNODB" ]; then
+            echo "INNODB_FILE_PER_TABLE is not enabled"
+            exit 1
+        fi
     fi
 fi
+
 exit 0
