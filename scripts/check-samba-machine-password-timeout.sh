7ccec3d59013fa36f756349612dc9e2ea49d57ad
Julien Semaan <jul.semaan@gmail.com>
2016-10-18 11:09:10 -0400

    added script to check machine password timeout

---
+++ scripts/check-samba-machine-password-timeout.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#fname:check-samba-machine-password-timeout.sh
+
+domains=`perl -I/usr/local/pf/lib -Mpf::config -e "print join(' ', keys(%pf::config::ConfigDomain))"`
+
+error=0
+
+for domain in $domains; do
+  if ! grep --quiet '^machine password timeout = 0' /etc/samba/$domain.conf; then
+    echo "Machine password timeout isn't disabled for $domain"
+    error=1
+  fi
+done
+
+exit $error
+
