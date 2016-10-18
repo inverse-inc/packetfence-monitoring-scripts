de6c6289d5b3369393acad98e3f99b2b4b6aa8b9
Julien Semaan <jul.semaan@gmail.com>
2016-10-18 12:55:18 -0400

    check tmp files count with env override

--- scripts/check-tmp.sh
+++ scripts/check-tmp.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 #fname:check-tmp.sh
 
-if [ `find /tmp/ -type f | wc -l` -gt 1000 ] ; then 
+MAX_TMP_FILES="${MAX_TMP_FILES:-"1000"}"
+
+if [ `find /tmp/ -type f | wc -l` -gt $MAX_TMP_FILES ] ; then
   echo "There are too much temp files in /tmp"
   exit 1
 fi
