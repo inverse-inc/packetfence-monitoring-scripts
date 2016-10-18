314c5f9f950b365d2541fe074c2be72772361855
Julien Semaan <jul.semaan@gmail.com>
2016-10-18 14:16:56 -0400

    fix check-tmp.sh script depth

--- scripts/check-tmp.sh
+++ scripts/check-tmp.sh
@@ -3,7 +3,7 @@
 
 MAX_TMP_FILES="${MAX_TMP_FILES:-"1000"}"
 
-if [ `find /tmp/ depth 1 -type f | wc -l` -gt $MAX_TMP_FILES ] ; then
+if [ `find /tmp/ -maxdepth 1 -type f | wc -l` -gt $MAX_TMP_FILES ] ; then
   echo "There are too much temp files in /tmp"
   exit 1
 fi
