9708e623d1576a5babec5092043fad5a56235796
Julien Semaan <jul.semaan@gmail.com>
2016-10-18 11:28:20 -0400

    improvements to drbd script check

--- scripts/check-drbd-status.sh
+++ scripts/check-drbd-status.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 #fname:check-drbd-status.sh
 
+if ! [ -f /proc/drbd ] ; then
+    exit 0
+fi
+
 # Get DRBD status
+drbd_output=""
 status=$(egrep "(Primary\/Secondary|Secondary\/Primary)" /proc/drbd | egrep -o UpToDate/UpToDate | wc -l)
 cat /proc/drbd >> $drbd_output
 
