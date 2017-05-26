1d90161b6d261b2256f30f706e3504d677b2634d
Julien Semaan <jul.semaan@gmail.com>
2017-05-26 09:39:46 -0400

    fix relative path in check-2358.sh

--- scripts/check-2358.sh
+++ scripts/check-2358.sh
@@ -5,7 +5,7 @@ pfversion
 
 if [ "$MAINTENANCE_VERSION" = "7.0" ]; then
   if /usr/local/pf/bin/pfcmd pfconfig show config::Pfmon | grep "'rotate'" | egrep '(Y|enabled)' > /dev/null; then
-    if ! grep 'rotate_' lib/pfconfig/namespaces/config/Pfmon.pm > /dev/null; then
+    if ! grep 'rotate_' /usr/local/pf/lib/pfconfig/namespaces/config/Pfmon.pm > /dev/null; then
       echo "ip4log or ip6log rotation is enabled and the setup is affected by bugs #2358 and #2359\n"
       echo "Apply the maintenance branch (/usr/local/pf/addons/pf-maint.pl) and restart packetfence-config and pfmon to fix this issue"
       exit 1
