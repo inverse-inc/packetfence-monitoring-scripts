a75b6bf63d5a85e444954933178fb4ffd8bede49
Julien Semaan <jul.semaan@gmail.com>
2017-05-26 12:50:41 -0400

    change comparison operator

--- scripts/check-2358.sh
+++ scripts/check-2358.sh
@@ -3,7 +3,7 @@
 
 pfversion
 
-if [ "$MAINTENANCE_VERSION" = "7.0" ]; then
+if [ "$MAINTENANCE_VERSION" == "7.0" ]; then
   if /usr/local/pf/bin/pfcmd pfconfig show config::Pfmon | grep "'rotate'" | egrep '(Y|enabled)' > /dev/null; then
     if ! grep 'rotate_' /usr/local/pf/lib/pfconfig/namespaces/config/Pfmon.pm > /dev/null; then
       echo "ip4log or ip6log rotation is enabled and the setup is affected by bugs #2358 and #2359\n"
