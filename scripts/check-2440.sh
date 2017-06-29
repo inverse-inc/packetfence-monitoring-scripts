c656bb0018d4c0dca05da44ed8494cc5501768eb
Julien Semaan <jul.semaan@gmail.com>
2017-06-29 13:32:25 -0400

    Update check-2440.sh

--- scripts/check-2440.sh
+++ scripts/check-2440.sh
@@ -4,7 +4,7 @@
 pfversion
 
 if [ "$MAINTENANCE_VERSION" = "7.0" ] || [ "$MAINTENANCE_VERSION" = "7.1" ]; then
-  if [ -f /var/lib/mysql/grastate.dat ]; then
+  if galera_enabled; then
     if ! grep expire_logs_days /usr/local/pf/var/conf/mariadb.conf > /dev/null; then
       echo "There is no limit for the binary logs of Galera cluster\n"
       echo "This can lead to a lot of disk space usage in /var/lib/mysql/ and potentially to the disk being full\n"
