29699f259d105e432908aae80e4e2863ab8a8307
Julien Semaan <jul.semaan@gmail.com>
2017-06-30 11:29:02 -0400

    fix path

--- scripts/check-2388.sh
+++ scripts/check-2388.sh
@@ -5,7 +5,7 @@ pfversion
 
 if [ "$MAINTENANCE_VERSION" = "7.0" ] || [ "$MAINTENANCE_VERSION" = "7.1" ]; then
   if galera_enabled; then
-    if ! grep IS_CLUSTER addons/database-backup-and-maintenance.sh > /dev/null; then
+    if ! grep IS_CLUSTER /usr/local/pf/addons/database-backup-and-maintenance.sh > /dev/null; then
       echo "Database script may freeze Galera cluster during nightly backups (see #2388)\n"
       echo "Apply the maintenance branch (/usr/local/pf/addons/pf-maint.pl) to fix this issue"
       exit 1
