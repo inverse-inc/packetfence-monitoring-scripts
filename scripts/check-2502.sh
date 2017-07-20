432e5cfe40de9df1e315e2926df1e9efe451a535
Julien Semaan <jul.semaan@gmail.com>
2017-07-20 10:55:44 -0400

    Adjust variable extraction

--- scripts/check-2502.sh
+++ scripts/check-2502.sh
@@ -7,14 +7,14 @@ if ! [ -z "$(command -v mysql)" ]; then
     # We check for the packetfence-mariadb PID which means only versions 7.0 and above will use this script
     if [ -f /var/lib/mysql/`hostname`.pid ]; then
         dbparams
-        MAX_CONN=`mysql -h $DB_HOST -u $DB_USER -p$DB_PWD -D $DB_NAME -e "show variables like 'max_connections'" | tail -1 | awk '{ print $2 }'`
+        MAX_CONN=`mysql -h $DB_HOST -u $DB_USER -p$DB_PWD -D $DB_NAME -s -N -e "show variables like 'max_connections'" | awk '{ print $2 }'`
         if [ $MAX_CONN -eq 214 ]; then
           echo "MySQL is using the default number of connections (214) which means the setting database_advanced.max_connections may be ignored\n"
           echo "See this for resolution: https://github.com/inverse-inc/packetfence/issues/2502 or contact the PacketFence support for help"
           exit 1
         fi
-        echo $MAX_CONN
     fi
 fi
 
 exit 0
+
