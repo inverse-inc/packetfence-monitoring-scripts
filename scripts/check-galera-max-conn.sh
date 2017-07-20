aee2a41ac6b85dfe5bf26ac00a6ea6e45a7ea0aa
Julien Semaan <jul.semaan@gmail.com>
2017-07-20 10:56:49 -0400

    Adjust variable extraction

--- scripts/check-galera-max-conn.sh
+++ scripts/check-galera-max-conn.sh
@@ -8,14 +8,13 @@ if ! [ -z "$(command -v mysql)" ]; then
     # We also check if Galera is enabled
     if [ -f /var/lib/mysql/`hostname`.pid ] && galera_enabled; then
         dbparams
-        MAX_CONN=`mysql -h $DB_HOST -u $DB_USER -p$DB_PWD -D $DB_NAME -e "show variables like 'max_connections'" | tail -1 | awk '{ print $2 }'`
+        MAX_CONN=`mysql -h $DB_HOST -u $DB_USER -p$DB_PWD -D $DB_NAME -s -N -e "show variables like 'max_connections'" | awk '{ print $2 }'`
         if [ $MAX_CONN -lt 1000 ]; then
           echo "You are using less than 1000 connections ($MAX_CONN) for MySQL while having Galera cluster enabled\n"
           echo "This is known to cause potential issues under high load.\n"
           echo "We suggest you increase the amount of connections in database_advanced.max_connections and restart packetfence-mariadb"
           exit 1
         fi
-        echo $MAX_CONN
     fi
 fi
 
