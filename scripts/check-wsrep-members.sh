4ac67792af29359d2aa19b38a7251441b6c41542
Julien Semaan <jul.semaan@gmail.com>
2017-08-28 15:10:33 -0400

    super :nail_care: 2000
    you've never seen something this polished

--- scripts/check-wsrep-members.sh
+++ scripts/check-wsrep-members.sh
@@ -4,9 +4,9 @@
 if ! [ -z "$(command -v mysql)" ]; then
 
     # is MariaDB running? meaning we are the live packetfence
-    # We check for the packetfence-mariadb PID which means only versions 7.0 and above will use this script
+    # We check for the packetfence-mariadb which means only versions 7.0 and above will use this script
     # We also check if Galera is enabled
-    if [ -f /var/lib/mysql/`hostname`.pid ] && galera_enabled; then
+    if packetfence_mariadb_running && galera_enabled; then
         dbparams
         cluster_members_count
         MEMBERS=`mysql -h $DB_HOST -u $DB_USER -p$DB_PWD -D $DB_NAME -s -N -e "show status like 'wsrep_incoming_addresses';" | awk '{ print $2 }'`
@@ -14,10 +14,12 @@ if ! [ -z "$(command -v mysql)" ]; then
         COUNT=`expr $COUNT + 1`
         if [ $COUNT -lt $CLUSTER_MEMBERS_COUNT ]; then
             echo "Wrong amount of WSREP cluster members $COUNT vs required $CLUSTER_MEMBERS_COUNT \n"
-            echo "List of current members: $MEMBERS"
+            echo "List of current members: $MEMBERS \n"
+            echo "List of expected members: $CLUSTER_MEMBERS"
             exit 1
         fi
     fi
 fi
 
 exit 0
+
